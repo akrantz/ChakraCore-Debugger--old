030203de0f4a835801c8522d8fc7fa80f79dfc96
Kyle Farnung <kfarnung@microsoft.com>
2018-03-26 17:56:56 -0700

    Enable end-to-end connection flow
    * Added class to manage connections to a protocol handler
    * Filled out the required string API interfaces to enable request/
      response flow
    * Added a call to wait for the debugger connection
    * Replaced the sample with a more complex ChakraCore sample
    * Fixed whitespace consistency

--- lib/Debug.ProtocolHandler/stdafx.h
+++ lib/Debug.ProtocolHandler/stdafx.h
@@ -10,3 +10,13 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
 #include <cstdint>
+#include <exception>
+#include <iostream>
+#include <memory>
+
+#include <ChakraCore.h>
+
+inline void IfJsErrorThrow(JsErrorCode error, const char* message)
+{
+    throw std::runtime_error(message);
+}
