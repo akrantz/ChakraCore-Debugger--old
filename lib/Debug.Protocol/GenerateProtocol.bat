e280e8346b81ff190d292f17462aba9b8dba4cbc
Adam Krantz <Adam.Krantz@microsoft.com>
2018-05-11 09:03:29 -0700

    Ensure empty generated dir exists

--- lib/Debug.Protocol/GenerateProtocol.bat
+++ lib/Debug.Protocol/GenerateProtocol.bat
@@ -3,6 +3,8 @@ setlocal
 
 set DEPS=..\\..\\deps
 
-if not exist "generated" mkdir generated
+if exist generated rmdir /s /q generated
+mkdir generated
 
 python "GenerateProtocol.py" --generator_script "%DEPS%\\inspector_protocol\\CodeGenerator.py" --jinja_dir "%DEPS%\\jinja\\" --markupsafe_dir "%DEPS%\\markupsafe\\" --output_base "generated\\" --config "inspector_protocol_config.json"
+
